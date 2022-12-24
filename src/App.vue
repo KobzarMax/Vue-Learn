b348189a32ab4f9ce70908b7990009d45a47f669
Max Kobzar <nimbus2219@gmail.com>
2022-12-24 20:49:38 +0100

    notes-app

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,160 @@
 <script setup>
   import { ref } from 'vue'
 
-  const count = ref(0);
+  const showModal = ref(false);
+  const newNote = ref("");
+  const notes = ref([]);
+
+  const errorMessage = ref("");
+
+  function randomColor() {
+    return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+  }
+
+  const addNote = () => {
+    if(newNote.value.length < 10) {
+      return errorMessage.value = "Name of note need to have at least 10 characters"
+    }
+    notes.value.push({
+      id: Math.floor(Math.random() * 2219),
+      text: newNote.value,
+      date: new Date(),
+      backgroundColor: randomColor(),
+    });
+    showModal.value = false;
+    newNote.value = "";
+    errorMessage.value = "";
+  }
+
 </script>
 
 <template>
-      <div class="wrapper">
-          <h4>Current count is {{ count }}</h4>
-          <h1 class="count">
-              {{ count }}
-          </h1>
-          <div>
-            <button class="minus" @click="count--">-</button>
-            <button class="add" @click="count++">+</button>
-          </div>
+  <main>
+    <div v-if="showModal" class="overlay">
+      <div class="modal">
+        <textarea placeholder="Write a note ..." v-model.trim="newNote" name="notes" id="notes" cols="30" rows="10"></textarea>
+        <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+        <button @click="addNote()">
+          Add Note
+        </button>
+        <button class="close" @click="showModal = false, errorMessage = ''">
+          Close
+        </button>
       </div>
+    </div>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button @click="showModal = true, errorMessage = ''">+</button>
+      </header>
+      <div class="cards-container">
+        <div v-for="note in notes" :key="note.id" class="card" :style="{backgroundColor: note.backgroundColor}">
+          <p class="main-text">
+            {{ note.text }}
+          </p>
+          <p class="date">
+            {{ note.date.toLocaleDateString("en-US") }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
+  main {
+    height: 100vh;
+    width: 100vw;
   }
-  .wrapper {
+  .container {
+    max-width: 1000px;
+    padding: 10px;
+    margin: 0 auto;
+  }
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  h1 {
+    font-weight: 700;
+    margin-bottom: 25px;
+    font-size: 75px;
+  }
+  header button {
+    background-color: black;
+    color: white;
+    font-weight: 700;
+    font-size: 20px;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    border-radius: 50%;
+    border: none;
+  }
+  .card {
+    width: 225px;
+    height: 225px;
+    background-color: aquamarine;
+    padding: 10px;
+    border-radius: 16px;
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-right: 20px;
+    margin-bottom: 20px;
+  }
+  .date {
+    font-size: 12px;
+    font-weight: 600;
+  }
+  .cards-container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+  .overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 10;
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+  .modal {
+    width: 750px;
+    background-color: aliceblue;
+    border-radius: 10px;
+    padding: 30px;
+    border: 2px solid black;
+    gap: 10px;
+    position: relative;
+    display: flex;
     flex-direction: column;
   }
-
-  button {
+  .modal button {
     background-color: brown;
-    width: 100px;
-    height: 40px;
-    border-radius: 16px;
     color: white;
-    font-size: 24px;
     font-weight: 700;
-    margin-right: 10px;
+    font-size: 24px;
+    padding: 10px 20px;
+    cursor: pointer;
+    border: none;
+    border-radius: 16px;
   }
-
-  h1, h4 {
-    text-align: center;
+  .modal .close {
+    background-color: red;
+  }
+  textarea {
+    font-size: 18px;
+    padding: 20px;
+  }
+  .error-message {
+    color: red;
+    font-size: 18px;
   }
 </style>
 
